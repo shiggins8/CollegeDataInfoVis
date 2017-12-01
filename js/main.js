c36a914b1d271070e60bf730f3b624643a36d11a
Scott Higgins <skoty.higgins@gmail.com>
2017-12-01 12:52:05 -0500

    Added d3.queue() functionality to allow datasets to load in the right order

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@ var projection = d3.geoAlbersUsa()
 var path = d3.geoPath()
     .projection(projection);
 
-d3.json("./data/us.json", function(error, us) {
+function drawTheMap(us) {
     var map = svg.append('g')
         .attr('class', 'mapOfStates');
 
@@ -101,7 +101,7 @@ d3.json("./data/us.json", function(error, us) {
         .attr("class", "states")
         .datum(topojson.feature(us, us.objects.states))
         .attr("d", path);
-});
+}
 
 var legend = svg.append('g')
         .attr('transform', 'translate(1135,490)');
@@ -159,64 +159,66 @@ svg.append("text")
     .attr('class', 'authors')
     .text('Scott Higgins');
 
+d3.queue()
+    .defer(d3.json, './data/us.json')
+    .defer(d3.csv, './data/colleges.csv', function(row) {
+        return {
+            name: row['Name'],
+            admission_rate_show: true,
+            act_median_show: true,
+            sat_average_show: true,
+            undergrad_pop_show: true,
+            latitude: +row['Latitude'],
+            longitude: +row['Longitude'],
+            control: row['Control'],
+            region: row['Region'],
+            locale: row['Locale'],
+            admission_rate: +row['Admission Rate'],
+            act_median: +row['ACT Median'],
+            sat_average: +row['SAT Average'],
+            undergrad_pop: +row['Undergrad Population'],
+            percent_white: +row['% White'],
+            percent_black: +row['% Black'],
+            percent_hispanic: +row['% Hispanic'],
+            percent_asian: +row['% Asian'],
+            percent_amer_indian: +row['% American Indian'],
+            percent_pacific_islander: +row['% Pacific Islander'],
+            percent_biracial: +row['% Biracial'],
+            percent_aliens: +row['% Nonresident Aliens'],
+            percent_part_time_students: +row['% Part-time Undergrads'],
+            average_cost: +row['Average Cost'],
+            expenditure_per_student: +row['Expenditure Per Student'],
+            average_faculty_salary: +row['Average Faculty Salary'],
+            percent_full_time_faculty: +row['% Full-time Faculty'],
+            percent_students_with_pell: +row['% Undergrads with Pell Grant'],
+            completion_rate: +row['Completion Rate 150% time'],
+            retention_rate: +row['Retention Rate (First Time Students)'],
+            percent_older_students: +row['% Undergrads 25+ y.o.'],
+            three_year_default_rate: +row['3 Year Default Rate'],
+            median_debt: +row['Median Debt'],
+            median_debt_on_graduation: +row['Median Debt on Graduation'],
+            median_debt_on_withdrawal: +row['Median Debt on Withdrawal'],
+            percent_federal_loans: +row['% Federal Loans'],
+            percent_pell_recipients: +row['% Pell Grant Recipients'],
+            average_entry_age: +row['Average Age of Entry'],
+            average_family_income: +row['Average Family Income'],
+            median_family_income: +row['Median Family Income'],
+            poverty_rate: +row['Poverty Rate'],
+            unemployed_after_eight: +row['Number of Unemployed 8 years after entry'],
+            employed_after_eight: +row['Number of Employed 8 years after entry'],
+            mean_earnings_after_eight: +row['Mean Earnings 8 years After Entry'],
+            median_earnings_after_eight: +row['Median Earnings 8 years After Entry']
+        }
+    })
+    .await(drawEverything);
 
-// College data
-d3.csv('./data/colleges.csv',
-function(row){
-    return {
-        name: row['Name'],
-        admission_rate_show: true,
-        act_median_show: true,
-        sat_average_show: true,
-        undergrad_pop_show: true,
-        latitude: +row['Latitude'],
-        longitude: +row['Longitude'],
-        control: row['Control'],
-        region: row['Region'],
-        locale: row['Locale'],
-        admission_rate: +row['Admission Rate'],
-        act_median: +row['ACT Median'],
-        sat_average: +row['SAT Average'],
-        undergrad_pop: +row['Undergrad Population'],
-        percent_white: +row['% White'],
-        percent_black: +row['% Black'],
-        percent_hispanic: +row['% Hispanic'],
-        percent_asian: +row['% Asian'],
-        percent_amer_indian: +row['% American Indian'],
-        percent_pacific_islander: +row['% Pacific Islander'],
-        percent_biracial: +row['% Biracial'],
-        percent_aliens: +row['% Nonresident Aliens'],
-        percent_part_time_students: +row['% Part-time Undergrads'],
-        average_cost: +row['Average Cost'],
-        expenditure_per_student: +row['Expenditure Per Student'],
-        average_faculty_salary: +row['Average Faculty Salary'],
-        percent_full_time_faculty: +row['% Full-time Faculty'],
-        percent_students_with_pell: +row['% Undergrads with Pell Grant'],
-        completion_rate: +row['Completion Rate 150% time'],
-        retention_rate: +row['Retention Rate (First Time Students)'],
-        percent_older_students: +row['% Undergrads 25+ y.o.'],
-        three_year_default_rate: +row['3 Year Default Rate'],
-        median_debt: +row['Median Debt'],
-        median_debt_on_graduation: +row['Median Debt on Graduation'],
-        median_debt_on_withdrawal: +row['Median Debt on Withdrawal'],
-        percent_federal_loans: +row['% Federal Loans'],
-        percent_pell_recipients: +row['% Pell Grant Recipients'],
-        average_entry_age: +row['Average Age of Entry'],
-        average_family_income: +row['Average Family Income'],
-        median_family_income: +row['Median Family Income'],
-        poverty_rate: +row['Poverty Rate'],
-        unemployed_after_eight: +row['Number of Unemployed 8 years after entry'],
-        employed_after_eight: +row['Number of Employed 8 years after entry'],
-        mean_earnings_after_eight: +row['Mean Earnings 8 years After Entry'],
-        median_earnings_after_eight: +row['Median Earnings 8 years After Entry']
-    }
-},
-function(error, dataset){
+function drawEverything(error, us, dataset) {
     if(error) {
-        console.error('Error while loading ./data/colleges.csv dataset.');
+        console.error('Error while loading datasets.');
         console.error(error);
         return;
     }
+    drawTheMap(us);
 
     //filters out the colleges that are not in the US
     usColleges = dataset.filter(function(d) {
@@ -338,7 +340,7 @@ function(error, dataset){
 
     updateDots();
     updateList();
-});
+};
 
 function updateDots() {
     //checks to see if the show attribute is true and filters colleges
